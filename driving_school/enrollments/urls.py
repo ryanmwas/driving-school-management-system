3e5d48e794f036f64f5f59f45e0a1661868f41c8
Ryan <mwangiryan78@gmail.com>
2024-11-06 10:55:08 +0000

    add path for adding student in web interface

--- driving_school/enrollments/urls.py
+++ driving_school/enrollments/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('students/', views.student_list, name='student_list'),
     path('classes/', views.class_list, name='class_list'),
+    path('add_student/', views.add_student, name='add_student'),
 ]

2cb3b1f2d83724f7efd0a5aafd3820127092bb68
Ryan <mwangiryan78@gmail.com>
2024-11-06 10:55:09 +0000

    Hide add student feature unless loggedin

--- driving_school/enrollments/views.py
+++ driving_school/enrollments/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Student, ClassSchedule
+from django.contrib.auth.decorators import login_required
+from .forms import StudentForm
 
 def student_list(request):
     students = Student.objects.all() # retrieves all student objects from database
@@ -8,3 +10,14 @@ def student_list(request):
 def class_list(request):
     classes = ClassSchedule.objects.all() # retrieves all class schedule objects from database
     return render(request, 'enrollments/class_list.html', {'classes': classes})
+
+@login_required
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'enrollments/add_student.html', {'form': form})
